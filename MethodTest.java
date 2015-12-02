aa49c7bc639bb4bb00fd5ad6592f14e9b61cd1ca
dhruv-shah <dhruv.ilesh@gmail.com>
2015-12-02 17:08:42 +0530

    Adding my first terminal-based Java test files

---
+++ MethodTest.java
@@ -0,0 +1,13 @@
+public class MethodTest {
+
+public static int method1(){
+int a = 10;
+return a;
+}    
+public static void main(String[] args) { 
+        int i=0;
+	
+	
+	System.out.println(""+ method1());
+    }
+}
